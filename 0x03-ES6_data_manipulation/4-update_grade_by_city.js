156b6c4e402092b5c3a65b18beabcb1a110b252f
JOSEPH ANI <60388143+Nonso024@users.noreply.github.com>
2023-12-05 15:38:48 +0100

    Update 4-update_grade_by_city.js

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,7 +2,7 @@ export default function updateStudentGradeByCity(listOfStudents, city, newGrades
   return listOfStudents.filter((student) => student.location === city)
     .map((student) => {
       for (const newGrade of newGrades) {
-        if (student.id === newGrade.student) {
+        if (student.id === newGrade.studentId) {
           return { ...student, grade: newGrade.grade };
         }
       }
